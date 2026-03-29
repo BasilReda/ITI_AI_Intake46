230a4e954551166b134183cac1b001554a9d399c
basilreda <basilreda112211@gmail.com>
2026-03-29 06:49:54 +0200

    fixed logic error

--- DSA/day3/matrix_chain.py
+++ DSA/day3/matrix_chain.py
@@ -1,18 +1,15 @@
 def matrix_chain(dimensions):
     n = len(dimensions) - 1
     m = {(i, j): float("inf") for i in range(1, n+1) for j in range(i, n+1)}
-
-    for i in range(1, n+1):
-        m[(i,i)] = 0
     
-    for i in range(1, n+1):
-        for j in range(i, n+1):
-            if i == j:
-                m[(i,j)] = 0
-            else:
-                for k in range(i,j):
-                    cost = m[(i,k)] + m[(k+1,j)] + (dimensions[i-1] * dimensions[k] * dimensions[j])
-                    m[(i,j)] = min(cost, m[(i,j)])
+    for length in range(2, n+1):
+        for i in range(1, n-length+2):
+            j = i + length - 1
+            m[(i,j)] = float("inf")
+            for k in range(i,j):
+                cost = m[(i,k)] + m[(k+1,j)] + (dimensions[i-1] * dimensions[k] * dimensions[j])
+                if cost < m[(i,j)]:
+                    m[(i,j)] = cost
     return m[(1,n)]
 
 dimensions = [5, 20, 10, 50]
